be0d9a9d2ef2dcc5e79ce028bb51077bbdefc8f8
devon-wolf <devon.wolfkiel@gmail.com>
2022-06-17 19:04:36 -0700

    add dictionary and thesaurus lookups, test out dict

--- golookup.go
+++ golookup.go
@@ -3,20 +3,44 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	res, err := http.Get("https://magnus-archive.herokuapp.com/episodes")
+func init() {
+	err := godotenv.Load(".env")
 	if err != nil {
-		fmt.Printf("%s", err)
-		os.Exit(1)
+		log.Fatal("Error loading .env file")
 	}
+}
+
+func main() {
+	lookupUrl := makeDictionaryUrl("purple")
+	res, err := http.Get(lookupUrl)
+	printErrorAndExit(err)
 	resBody, bodyErr := ioutil.ReadAll(res.Body)
-	if bodyErr != nil {
-		fmt.Printf("%s", bodyErr)
+	printErrorAndExit(bodyErr)
+	fmt.Printf("%s", resBody)
+}
+
+func printErrorAndExit(err error) {
+	if err != nil {
+		fmt.Printf("error: %s", err)
 		os.Exit(1)
 	}
-	fmt.Printf("%s", resBody)
+}
+
+func makeDictionaryUrl(word string) string {
+	key := os.Getenv("DICTIONARY_KEY")
+	dictionaryUrl := fmt.Sprintf("https://www.dictionaryapi.com/api/v3/references/collegiate/json/%s?key=%s", word, key)
+	return dictionaryUrl
+}
+
+func makeThesaurusUrl(word string) string {
+	key := os.Getenv("THESAURUS_KEY")
+	thesaurusUrl := fmt.Sprintf("https://www.dictionaryapi.com/api/v3/references/thesaurus/json/%s?key=%s", word, key)
+	return thesaurusUrl
 }
